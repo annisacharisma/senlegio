bf77895f1edd166214dd571d91b5da7fac47211c
annisacharisma <129478564+annisacharisma@users.noreply.github.com>
2024-07-23 14:50:33 +0700

    Add files via upload

---
+++ application/views/templates/table_page.php
@@ -0,0 +1,35 @@
+<li class="nav-item menu">
+    <a href="#" class="nav-link">
+      <i class="nav-icon fas fa-edit"></i>
+      <p>
+        PROJECT PAGE
+        <i class="fas fa-angle-left right"></i>
+      </p>
+    </a>
+     <ul class="nav nav-treeview">
+      <li class="nav-item">
+        <a href="<?php echo base_url("c_data/table_advertising");?>" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>Advertising</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?php echo base_url("c_data/table_animatedshorts");?>" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>Animated Shorts</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?php echo base_url("c_data/table_visualeffects");?>" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>Visual Effects</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?php echo base_url("c_data/table_digiceleb");?>" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>Digital Celebrity</p>
+        </a>
+      </li>
+    </ul>
+  </li>
