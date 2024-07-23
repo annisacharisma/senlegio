ad2297c0611be1b66af77d1ef31c1c25f6c74d00
annisacharisma <129478564+annisacharisma@users.noreply.github.com>
2024-07-23 14:55:08 +0700

    Update v_admin_table_dc.php

--- application/views/v_admin_table_dc.php
+++ application/views/v_admin_table_dc.php
@@ -184,41 +184,9 @@
               </p>
             </a>
           </li>
-          <li class="nav-item menu">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Data Page
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Advertising</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?php echo base_url("c_data/data_animatedshorts");?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Animated Shorts</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?php echo base_url("c_data/data_visualeffects");?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Visual Effects</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?php echo base_url("c_data/data_digiceleb");?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Digital Celebrity</p>
-                </a>
-              </li>
-            </ul>  
-          </li>
+                    
+          <?php  $this->load->view('templates/data_page');?>
+          
           <li class="nav-item menu-open">
               <a href="#" class="nav-link active">
                 <i class="nav-icon fas fa-edit"></i>
