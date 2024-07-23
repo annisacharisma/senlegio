889586b9943974a6421670d260a08c1b13a906e5
annisacharisma <129478564+annisacharisma@users.noreply.github.com>
2024-07-23 14:50:09 +0700

    Update and rename aside.php to data_page.php

---
+++ application/views/templates/data_page.php
@@ -0,0 +1,35 @@
+<li class="nav-item menu">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-tachometer-alt"></i>
+    <p>
+      Data Page
+      <i class="fas fa-angle-left right"></i>
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="<?php echo base_url("c_data/data_advertising");?>" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Data Advertising</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="<?php echo base_url("c_data/data_animatedshorts");?>" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Data Animated Shorts</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="<?php echo base_url("c_data/data_visualeffects");?>" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Data Visual Effects</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="<?php echo base_url("c_data/data_digiceleb");?>" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Data Digital Celebrity</p>
+      </a>
+    </li>
+  </ul>  
+</li>
