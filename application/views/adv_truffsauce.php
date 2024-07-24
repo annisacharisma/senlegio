e5d23d6f30586215a7a8975cee3d2c0a8a90c175
annisacharisma <129478564+annisacharisma@users.noreply.github.com>
2024-07-24 14:47:38 +0700

    Update adv_truffsauce.php

--- application/views/adv_truffsauce.php
+++ application/views/adv_truffsauce.php
@@ -1,34 +1,32 @@
 <?php  $this->load->view('templates/Header');?>
-<section data-bs-version="5.1" class="info1 cid-ui5KXAvjPq" id="info1-1z">
-    
-
-    
-    
-    <div class="align-center container-fluid">
-        <div class="row justify-content-center">
-            <div class="col-12 col-lg-8">
-                <h3 class="mbr-section-title mb-4 mbr-fonts-style display-1"><strong>Truff Sauce</strong></h3>
-                
-                
-            </div>
-        </div>
+<?php 
+foreach($advertising as $u){ 
+  ?>
+  <section data-bs-version="5.1" class="info1 cid-ui5KXAvjPq" id="info1-1z" style="background-image: url('<?php echo base_url('assets/'.$u->bg_adv); ?>');">
+  <div class="align-center container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-12 col-lg-8">
+        <h3 class="mbr-section-title mb-4 mbr-fonts-style display-1"><strong><?php echo $u->title_adv ?></strong></h3>
+      </div>
     </div>
-</section>
+  </div>
+  </section>
 
-<section data-bs-version="5.1" class="gallery3 cid-ui5LGHIJF4" id="gallery03-23">
-    
-    
+
+  <section data-bs-version="5.1" class="gallery3 cid-ui5LGHIJF4" id="gallery03-23">
+
+
     <div class="container-fluid">
       <div class="row justify-content-center">
         <div class="col-12 content-head">
-          
+
         </div>
       </div>
       <div class="row">
         <div class="item features-image col-12 col-md-6 col-lg-3">
           <div class="item-wrapper">
             <div class="item-img">
-              <img src="<?php echo base_url(); ?>assets/images/stills-1.1.2-1101x826.png" title="" data-slide-to="0" data-bs-slide-to="0">
+              <img src="<?php echo base_url('assets/'.$u->image_adv1); ?>" title="" data-slide-to="0" data-bs-slide-to="0">
             </div>
             
           </div>
@@ -36,7 +34,7 @@
         <div class="item features-image col-12 col-md-6 col-lg-3">
           <div class="item-wrapper">
             <div class="item-img">
-              <img src="<?php echo base_url(); ?>assets/images/Stills_1.1.6.png" title="" data-slide-to="1" data-bs-slide-to="1">
+              <img src="<?php echo base_url('assets/'.$u->image_adv2); ?>" title="" data-slide-to="1" data-bs-slide-to="1">
             </div>
             
           </div>
@@ -44,7 +42,7 @@
         <div class="item features-image col-12 col-md-6 col-lg-3">
           <div class="item-wrapper">
             <div class="item-img">
-              <img src="<?php echo base_url(); ?>assets/images/Stills_1.1.3.png" title="" data-slide-to="2" data-bs-slide-to="2">
+              <img src="<?php echo base_url('assets/'.$u->image_adv3); ?>" title="" data-slide-to="2" data-bs-slide-to="2">
             </div>
             
           </div>
@@ -52,7 +50,7 @@
         <div class="item features-image col-12 col-md-6 col-lg-3">
           <div class="item-wrapper">
             <div class="item-img">
-              <img src="<?php echo base_url(); ?>assets/images/Stills_1.1.5.png" title="" data-slide-to="3" data-bs-slide-to="3">
+              <img src="<?php echo base_url('assets/'.$u->image_adv4); ?>" title="" data-slide-to="3" data-bs-slide-to="3">
             </div>
             
           </div>
@@ -61,26 +59,27 @@
     </div>
   </section>
 
-<section data-bs-version="5.1" class="image1 cid-ui5M78tW8c" id="image1-24">
+  <section data-bs-version="5.1" class="image1 cid-ui5M78tW8c" id="image1-24">
     <div class="container">
-        <div class="row align-items-center">
-            <div class="col-12 col-lg-6">
-                <div class="image-wrapper">
-                  <video class="custom-video-size" controls autoplay muted>
-                    <source src="<?php echo base_url(); ?>assets/images/Truff_Sauce_v3.mp4" type="video/mp4">
-                    Your browser does not support the video tag.
-                  </video>
-                </div>
+      <div class="row align-items-center">
+        <div class="col-12 col-lg-6">
+          <div class="image-wrapper">
+            <video class="custom-video-size" controls autoplay muted>
+              <source src="<?php echo base_url('assets/'.$u->video_adv); ?>" type="video/mp4">
+                Your browser does not support the video tag.
+              </video>
             </div>
-            <div class="col-12 col-lg">
-                <div class="text-wrapper">
-                    <h3 class="mbr-section-title mbr-fonts-style mb-3 display-5">
-                        <strong>TRUFF SAUCE</strong></h3>
-                    <p class="mbr-text mbr-fonts-style display-7">
-                        Introducing a vibrant and engaging advertising video created for Truff, a renowned sauce company. Their vision for this project was to create something refreshingly fun, and we are thrilled to share the results of our collaboration. Take a look at our work!</p>
-                </div>
+          </div>
+          <div class="col-12 col-lg">
+            <div class="text-wrapper">
+              <h3 class="mbr-section-title mbr-fonts-style mb-3 display-5">
+                <strong><?php echo $u->title_adv ?></strong></h3>
+                <p class="mbr-text mbr-fonts-style display-7">
+                <?php echo $u->description_adv ?></p>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</section>
-<?php  $this->load->view('templates/Footer');?>
+      </section>
+      <?php } ?>
+      <?php  $this->load->view('templates/Footer');?>
