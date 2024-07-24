6deb0593508f689cf31244adb002624fcc68b29a
annisacharisma <129478564+annisacharisma@users.noreply.github.com>
2024-07-24 15:07:18 +0700

    Update v_admin_digiceleb.php

--- application/views/v_admin_digiceleb.php
+++ application/views/v_admin_digiceleb.php
@@ -137,7 +137,7 @@
                         <td><?php echo $no++ ?></td>
                         <td><?php echo $u->id_dc ?></td>
                         <td><?php echo $u->title_dc ?></td>
-                        <td><img src="<?php echo base_url('assets/images/' . $u->poster_dc); ?>" alt="<?php echo $u->title_dc; ?>" style="width: 100px; height: auto;"></td>
+                        <td><img src="<?php echo base_url('assets//' . $u->poster_dc); ?>" alt="<?php echo $u->title_dc; ?>" style="width: 100px; height: auto;"></td>
                         <td>
                           <a href="<?php echo site_url('crud/edit/'.$u->id_dc); ?>" class="btn btn-warning a-btn-slide-text">
                             <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> 
