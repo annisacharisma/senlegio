abb9e350096e2c3bd5215caa9613229540444182
melisanyh <129590002+melisanyh@users.noreply.github.com>
2024-07-24 15:21:28 +0700

    Update digiceleb_adam.php

--- application/views/digiceleb_adam.php
+++ application/views/digiceleb_adam.php
@@ -1,42 +1,33 @@
 <?php  $this->load->view('templates/Header');?>
-
-<section data-bs-version="5.1" class="info1 cid-ui5JPqBsqg" id="info1-1t">
-    
-
-    
-    
+<?php 
+foreach ($digiceleb as $u){
+  ?>
+<section data-bs-version="5.1" class="info1 cid-ui5JPqBsqg" id="info1-1t" style="background-image: url('<?php echo base_url('assets/'.$u->bg_dc); ?>');">
     <div class="align-center container-fluid">
         <div class="row justify-content-center">
             <div class="col-12 col-lg-8">
                 <h3 class="mbr-section-title mb-4 mbr-fonts-style display-1">
-                    <strong>ADAM</strong></h3>
-                
-                
+                <strong><?php echo $u->title_dc ?></strong></h3>
             </div>
         </div>
     </div>
 </section>
 
 <section data-bs-version="5.1" class="image1 cid-ui5KqPrWQq" id="image1-1x">
-    
-
-    
-    
-
     <div class="container">
         <div class="row justify-items-center">
             <div class="col-12 col-lg-5" style="padding-top: 2rem; padding-left: 5rem">
                 <div class="image-wrapper">
-                    <img src="assets/images/Profile.png" style="height: 38rem; width: 27rem; object-fit: cover;">
+                    <img src="<?php echo base_url('assets/'.$u->image_dc1); ?>" style="height: 38rem; width: 27rem; object-fit: cover;">
                 </div>
             </div>
             <div class="col-12 col-lg-7">
                 <div class="text-wrapper">
-                    <h3 class="mbr-section-title mbr-fonts-style mb-1 display-4"><strong>Adam is the first Digiceleb created by Senlegio Studio.</strong></h3>
-                    <p class="mbr-text mbr-fonts-style display-7"><br>A 22-year-old actor within his universe, Adam works for Senlegio Studio and brings a unique charm to the digital realm. One of his standout abilities is breaking the fourth wall, allowing him to communicate directly with us, the animators, in the real world. This special skill adds a layer of interactivity and humor to his character, making him a captivating presence in the Digiceleb universe.<br><br>With his extroverted personality, intelligence, and good looks, Adam is often the target of playful pranks. He has a keen sense of fashion and a great sense of humor, making him relatable and entertaining to a wide audience. As the pioneering Digiceleb, Adam sets the stage for an exciting and dynamic digital world crafted entirely by Senlegio Studio's talented 3D artists.</p>
-                    <img src="assets/images/showcase - 02.png" style="height: 17rem; width: 37rem; object-fit: cover;">
+                    <h3 class="mbr-section-title mbr-fonts-style mb-1 display-4"><strong><?php echo $u->description_line1 ?></strong></h3>
+                    <p class="mbr-text mbr-fonts-style display-7"><br><?php echo $u->description_line2 ?><br><br><?php echo $u->description_line3 ?></p>
+                    <img src='<?php echo base_url('assets/'.$u->image_dc2); ?>' style="height: 17rem; width: 37rem; object-fit: cover;">
                     <span class="btn btn-social mailru m-2" style="padding: 0; gap:0;">
-                        <a href="https://www.instagram.com/adam_digiceleb/" style="display: flex; align-items: center; justify-content: center; width: 100%; height: 100%;">
+                        <a href="<?php echo $u->instagram ?>" style="display: flex; align-items: center; justify-content: center; width: 100%; height: 100%;">
                             <i class="socicon socicon-instagram"></i>
                         </a>
                     </span>
@@ -45,5 +36,5 @@
         </div>
     </div>
 </section>
-
+<?php } ?>
 <?php  $this->load->view('templates/Footer');?>
