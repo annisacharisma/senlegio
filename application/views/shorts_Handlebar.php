5e744a03b09c77bf509b503d64a161224af99759
annisacharisma <129478564+annisacharisma@users.noreply.github.com>
2024-07-24 14:49:32 +0700

    Update shorts_Handlebar.php

--- application/views/shorts_Handlebar.php
+++ application/views/shorts_Handlebar.php
@@ -1,132 +1,104 @@
-<?php  $this->load->view('templates/Header');?>
+<?php $this->load->view('templates/Header'); ?>
 
-<section data-bs-version="5.1" class="info1 cid-ui5PTRMjSO" id="info1-2c">
-    
-
-    
-    
-    <div class="align-center container-fluid">
-        <div class="row justify-content-center">
-            <div class="col-12 col-lg-8">
-                <h3 class="mbr-section-title mb-4 mbr-fonts-style display-1">
-                    <strong>Handlebar</strong></h3>
-                
-                
+<?php foreach($animated_shorts as $u) { ?>
+    <section data-bs-version="5.1" class="info1 cid-ui5PTRMjSO" id="info1-2c" style="background-image: url('<?php echo base_url('assets/' . $u->bg_as); ?>');">
+        <div class="align-center container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-8">
+                    <h3 class="mbr-section-title mb-4 mbr-fonts-style display-1"><strong><?php echo $u->title_as; ?></strong></h3>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<section data-bs-version="5.1" class="article13 cid-ui5PTS9D3U" id="article13-2d">
-    
-    
-    
-    <div class="container-fluid">
-        <div class="row justify-content-center">
-            <div class="card col-md-12 col-lg-10">
-                <div class="card-wrapper">
-                    <div class="card-box align-left">
-                        
-                        <p class="mbr-text mbr-fonts-style mt-4 display-7">Collaborating with Director David Sulpizio, we meticulously brought his creative vision to life. From the initial stages of pre-production to the final touches in post-production, we were entrusted with the opportunity to work on this animated short film, ensuring every detail was crafted to perfection.</p>
-                        
+    <section data-bs-version="5.1" class="article13 cid-ui5PTS9D3U" id="article13-2d">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="card col-md-12 col-lg-10">
+                    <div class="card-wrapper">
+                        <div class="card-box align-left">
+                            <p class="mbr-text mbr-fonts-style mt-4 display-7"><?php echo $u->description_as; ?></p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<section data-bs-version="5.1" class="image4 cid-ui5PTSsqjM" id="image4-2e">
-    
+    <section data-bs-version="5.1" class="image4 cid-ui5PTSsqjM" id="image4-2e">
+        <div class="container-fluid">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <?php
+                    $images = [
+                        $u->image_as1,
+                        $u->image_as2,
+                        $u->image_as3,
+                        $u->image_as4,
+                        $u->image_as5,
+                        $u->image_as6,
+                        $u->image_as7,
+                        $u->image_as8,
+                        $u->image_as9,
+                        $u->image_as10,
+                    ];
+                    $active_set = false;
 
-    
-    
+                    for ($i = 0; $i < count($images); $i += 2) {
+                        if (!empty($images[$i]) || !empty($images[$i + 1])) {
+                            $active_class = !$active_set ? ' active' : '';
+                            $active_set = true;
+                            echo "<div class='carousel-item{$active_class}'>
+                                    <div class='row d-flex justify-content-center align-items-center text-center'>";
 
-   <div class="container-fluid">
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="row d-flex justify-content-center align-items-center text-center">
-                        <div class="col-lg-6">
-                            <div class="image-wrapper">
-                                <img src="<?php echo base_url(); ?>assets/images/handlebar_images1.png" alt="Loading Image" style="max-width: 80%; height: auto; margin-right:20%;">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="image-wrapper">
-                                <img src="<?php echo base_url(); ?>assets/images/handlebar_images2.png" alt="Loading Image" style="max-width: 80%; height: auto; margin-left:20%;">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="row d-flex justify-content-center align-items-center text-center">
-                        <div class="col-lg-6">
-                            <div class="image-wrapper">
-                                <img src="<?php echo base_url(); ?>assets/images/handlebar_images3.png" alt="Loading Image" style="max-width: 80%; height: auto; margin-right:20%;">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="image-wrapper">
-                                <img src="<?php echo base_url(); ?>assets/images/handlebar_images4.png" alt="Loading Image" style="max-width: 80%; height: auto; margin-left:20%;">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="row d-flex justify-content-center align-items-center text-center">
-                        <div class="col-lg-6">
-                            <div class="image-wrapper">
-                                <img src="<?php echo base_url(); ?>assets/images/handlebar_images5.png" alt="Loading Image" style="max-width: 80%; height: auto; margin-right:20%;">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="image-wrapper">
-                                <img src="<?php echo base_url(); ?>assets/images/handlebar_images6.png" alt="Loading Image" style="max-width: 80%; height: auto; margin-left:20%;">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="row d-flex justify-content-center align-items-center text-center">
-                        <div class="col-lg-6">
-                            <div class="image-wrapper">
-                                <img src="<?php echo base_url(); ?>assets/images/handlebar_images7.png" alt="Loading Image" style="max-width: 80%; height: auto; margin-right:20%;">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="image-wrapper">
-                                <img src="<?php echo base_url(); ?>assets/images/handlebar_images8.png" alt="Loading Image" style="max-width: 80%; height: auto; margin-left:20%;">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <button class="carousel-control-prev" role="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true" style="filter: invert(0) grayscale(100%) brightness(0); width: 20px; height: 20px;"></span>
-                </button>
-                <button class="carousel-control-next" role="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true" style="filter: invert(0) grayscale(100%) brightness(0); width: 20px; height: 20px;"></span>
-                </button>
-            </div>                    
-        </div>        
-    </div>
-</section>
+                            if (!empty($images[$i])) {
+                                echo "<div class='col-lg-6 d-flex justify-content-center'>
+                                        <div class='image-wrapper'>
+                                            <img src='" . base_url('assets/' . $images[$i]) . "' alt='Loading Image' style='max-width: 80%; height: auto; margin-right:20%;'>
+                                        </div>
+                                    </div>";
+                            }
 
-<section data-bs-version="5.1" class="video2 cid-ui5PTSOUsV" id="video2-2f">
-    
-    
-    
-    <div class="container">
-        
-        <div class="row justify-content-center mt-4">
-            <div class="col-12 col-md-9 video-block">
-                <div class="video-wrapper"><iframe class="mbr-embedded-video" src="<?php echo base_url(); ?>assets/images/Truff_Sauce_v3.mp4" type="video/mp4" width="1280" height="720" frameborder="0" object-fit="cover" display="block" allowfullscreen></iframe></div>
-                
+                            if (!empty($images[$i + 1])) {
+                                echo "<div class='col-lg-6 d-flex justify-content-center'>
+                                        <div class='image-wrapper'>
+                                            <img src='" . base_url('assets/' . $images[$i + 1]) . "' alt='Loading Image' style='max-width: 80%; height: auto; margin-left:20%;'>
+                                        </div>
+                                    </div>";
+                            }
+
+                            echo "</div></div>";
+                        }
+                    }
+                    ?>
+                </div>
+                <div>
+                    <button class="carousel-control-prev" role="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true" style="filter: invert(0) grayscale(100%) brightness(0); width: 20px; height: 20px;"></span>
+                    </button>
+                    <button class="carousel-control-next" role="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true" style="filter: invert(0) grayscale(100%) brightness(0); width: 20px; height: 20px;"></span>
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<?php  $this->load->view('templates/Footer');?>
+    <section data-bs-version="5.1" class="video2 cid-ui5PTSOUsV" id="video2-2f">
+        <div class="container">
+            <div class="row justify-content-center mt-4">
+                <div class="col-12 col-md-9 video-block d-flex flex-column justify-content-center align-items-center">
+                    <div class="video-wrapper">
+                        <video class="custom-video-size" controls autoplay muted>
+                            <source src="<?php echo base_url('assets/'.$u->video_as); ?>" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php } ?>
+
+<?php $this->load->view('templates/Footer'); ?>
