e69ab934892f48be9b83f03ce3bfad5c0fc9a9d4
annisacharisma <129478564+annisacharisma@users.noreply.github.com>
2024-07-24 14:48:39 +0700

    Update animatedshorts.php

--- application/views/animatedshorts.php
+++ application/views/animatedshorts.php
@@ -36,6 +36,25 @@
 </section>
 
 <section data-bs-version="5.1" class="gallery3 cid-ui5WWCnxYJ" id="gallery03-2q">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <?php 
+                $no = 1;
+                foreach($datamu as $u){ 
+                  ?>
+                  <div class="col-12 col-lg-3 d-flex flex-column justify-content-center align-items-center">
+                    <div class="image-wrapper">
+                        <a href="<?php echo site_url('c_senlegio/show_as_project/'.$u->id_as); ?>"><img src="<?php echo base_url('assets/'.$u->poster_as); ?>" alt="Image" style="height: 19rem; width: 15rem; object-fit: cover;"></a>
+                        <p class="mbr-text mbr-fonts-style mt-2 align-center display-7">
+                            <strong><?php echo $u->title_as ?></strong></p>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+</section>
+
+<!-- <section data-bs-version="5.1" class="gallery3 cid-ui5WWCnxYJ" id="gallery03-2q">
     
     
     <div class="container-fluid">
@@ -79,6 +98,6 @@
         
       </div>
     </div>
-  </section>
+  </section> -->
 
   <?php  $this->load->view('templates/Footer');?>
