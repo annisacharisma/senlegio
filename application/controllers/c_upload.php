9ae114ce5ed9eb7843418df45139b1b72622b7df
gin-na <132482153+gin-na@users.noreply.github.com>
2024-07-26 16:03:39 +0700

    Update function u/ adv c_upload.php

--- application/controllers/c_upload.php
+++ application/controllers/c_upload.php
@@ -2,42 +2,73 @@
 
 class C_upload extends CI_Controller {
 
-
-    function proses()
+    public function proses()
     {
-        $config['upload_path']          = './assets/images';
-        $config['allowed_types']        = 'jpg|png|mp4';
-        $config['encrypt_name']         = TRUE;
-        $this->load->library('upload', $config);
-        if ( ! $this->upload->do_upload('poster_adv'))
-        {
-            $error = array('error' => $this->upload->display_errors());
-            $this->load->view('v_admin_table_adv', $error);
-        }
-        else
-        {
-        $upload_data = $this->upload->data();
-        $data['title_adv'] = $this->input->post('title_adv');
-        $data['poster_adv'] = $upload_data['file_name'];
-        $data['bg_adv'] = $upload_data['file_name'];
-        $data['description_adv'] = $this->input->post('description_adv');
-        $data['image_adv1'] = $upload_data['file_name'];
-        $data['image_adv2'] = $upload_data['file_name'];
-        $data['image_adv3'] = $upload_data['file_name'];
-        $data['image_adv4'] = $upload_data['file_name'];
-        $data['video_adv'] = $upload_data['file_name'];
-        
-        $this->db->insert('advertising', $data);
-        redirect('c_upload');
-        }
-    }
+        $this->load->library('upload');
 
+        // Configure upload for each file
+        $config['upload_path'] = './assets/images/';
+        $config['allowed_types'] = 'jpg|png|mp4';
+        $config['encrypt_name'] = TRUE;
+
+        $this->upload->initialize($config);
+
+        $data = [];
+        $data['title_adv'] = $this->input->post('title_adv');
+        $data['description_adv'] = $this->input->post('description_adv');
+
+        // Upload and save poster_adv
+        if ($this->upload->do_upload('poster_adv')) {
+            $upload_data = $this->upload->data();
+            $data['poster_adv'] = $upload_data['file_name'];
+        }
+
+        // Upload and save bg_adv
+        if ($this->upload->do_upload('bg_adv')) {
+            $upload_data = $this->upload->data();
+            $data['bg_adv'] = $upload_data['file_name'];
+        }
+
+        // Upload and save image_adv1
+        if ($this->upload->do_upload('image_adv1')) {
+            $upload_data = $this->upload->data();
+            $data['image_adv1'] = $upload_data['file_name'];
+        }
+
+        // Upload and save image_adv2
+        if ($this->upload->do_upload('image_adv2')) {
+            $upload_data = $this->upload->data();
+            $data['image_adv2'] = $upload_data['file_name'];
+        }
+
+        // Upload and save image_adv3
+        if ($this->upload->do_upload('image_adv3')) {
+            $upload_data = $this->upload->data();
+            $data['image_adv3'] = $upload_data['file_name'];
+        }
+
+        // Upload and save image_adv4
+        if ($this->upload->do_upload('image_adv4')) {
+            $upload_data = $this->upload->data();
+            $data['image_adv4'] = $upload_data['file_name'];
+        }
+
+        // Upload and save video_adv
+        if ($this->upload->do_upload('video_adv')) {
+            $upload_data = $this->upload->data();
+            $data['video_adv'] = $upload_data['file_name'];
+        }
+
+        // Insert data into the database
+        $this->db->insert('advertising', $data);
+
+        // Redirect to the main page
+        redirect('c_upload');
+    }
 
     public function index()
     {
         $data['poster_adv'] = $this->db->get('advertising');
-        $this->load->view('v_admin_advertising',$data);
+        $this->load->view('v_admin_advertising', $data);
     }
 }
-
-
