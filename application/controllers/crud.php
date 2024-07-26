8cc8a0a5435b6a465df06eccbe645a8193783629
melisanyh <129590002+melisanyh@users.noreply.github.com>
2024-07-26 16:05:52 +0700

    Update crud.php

--- application/controllers/crud.php
+++ application/controllers/crud.php
@@ -1,44 +1,39 @@
-<?php 
- 
+<?php
+
 class Crud extends CI_Controller {
-    
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('Crud_model');
+        $this->load->helper('url');
+        $this->load->helper('file'); // Memuat helper file
+    }
+
+    // Method untuk menghapus data advertising
     public function hapus_ad($id_adv) {
-        // Load model
-        $this->load->model('Crud_model');
-        
-        // Call the delete function from the model
         $this->Crud_model->delete_ad($id_adv);
-        
-        // Redirect to the list page or any other page
-        redirect('path_to_redirect_after_delete');
+        $this->session->set_flashdata('message', 'Data berhasil dihapus');
+        redirect('c_admin2/data_advertising');
     }
 
+    // Method untuk menghapus animated shorts
     public function hapus_as($id_as) {
-
-        $this->load->model('Crud_model');
-        
         $this->Crud_model->delete_as($id_as);
-        
-        redirect('path_to_redirect_after_delete');
+        $this->session->set_flashdata('message', 'Data berhasil dihapus');
+        redirect('c_admin2/data_animatedshorts');
     }
 
+    // Method untuk menghapus visual effects
     public function hapus_ve($id_ve) {
-
-        $this->load->model('Crud_model');
-        
         $this->Crud_model->delete_ve($id_ve);
-        
-        redirect('path_to_redirect_after_delete');
+        $this->session->set_flashdata('message', 'Data berhasil dihapus');
+        redirect('c_admin2/data_visualeffects');
     }
 
+    // Method untuk menghapus digital celebrity
     public function hapus_dc($id_dc) {
-
-        $this->load->model('Crud_model');
-        
         $this->Crud_model->delete_dc($id_dc);
-        
-        redirect('path_to_redirect_after_delete');
+        $this->session->set_flashdata('message', 'Data berhasil dihapus');
+        redirect('c_admin2/data_digiceleb');
     }
 }
-
- 
