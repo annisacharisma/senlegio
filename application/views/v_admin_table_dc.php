66ddf2e8a86dc2fa2bd9c75a47bd9b532b8766bd
gin-na <132482153+gin-na@users.noreply.github.com>
2024-07-26 22:41:21 +0700

    Update input v_admin_table_dc.php

--- application/views/v_admin_table_dc.php
+++ application/views/v_admin_table_dc.php
@@ -112,75 +112,101 @@
 
       <!-- Main content -->
       <section class="content">
-        <div class="row">
-          <div class="col-md-10">
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">General</h3>
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-info">
+                <div class="card-header">
+                    <h3 class="card-title">Please fill out this form:</h3>
                 </div>
-              </div>
-              <div class="card-body">
-                <form id="fileForm" action="#" method="post" enctype="multipart/form-data">
-                  <div class="form-group">
-                    <label for="inputName">Project Name</label>
-                    <input type="text" id="project" class="form-control" name="project" placeholder="Enter title"
-                      required>
-                  </div>
-                  <div class="form-group">
-                    <label for="inputDescription">Project Description</label>
-                    <textarea id="description" class="form-control" rows="4" name="description"
-                      placeholder="Enter description" required></textarea>
-                  </div>
-                  <div class="form-group">
-                    <label for="inputig">Instagram</label>
-                    <input type="text" id="instagram" class="form-control" name="instagram"
-                      placeholder="Enter instagram link" required>
-                  </div>
-                  <div class="form-group">
-                    <label for="inputDescription">Description Title</label>
-                    <textarea id="description" class="form-control" rows="4" name="description1"
-                      placeholder="Enter description" required></textarea>
-                  </div>
-                  <div class="form-group">
-                    <label for="inputDescription">Project Description</label>
-                    <textarea id="description" class="form-control" rows="4" name="description2"
-                      placeholder="Enter description" required></textarea>
-                  </div>
-                  <div class="form-group">
-                    <label for="poster">Choose Poster</label>
-                    <input type="file" class="form-control" id="poster" name="poster" required>
-                  </div>
-                  <div class="form-group">
-                    <label for="background">Choose Background Title</label>
-                    <input type="file" class="form-control" id="background" name="background" required>
-                  </div>
-                  <div class="form-group">
-                    <label for="files">Choose images 1</label>
-                    <input type="file" class="form-control" id="files" name="image1" required>
-                  </div>
-                  <div class="form-group">
-                    <label for="files">Choose images 2</label>
-                    <input type="file" class="form-control" id="files" name="image2" required>
-                  </div>
-                  <input type="submit" value="Save Changes" class="btn btn-success float-right">
+                <?php 
+                if(isset($error))
+                {
+                    echo "ERROR UPLOAD : <br/>";
+                    print_r($error);
+                    echo "<hr/>";
+                }
+                ?>
+                <form method="post" enctype="multipart/form-data" action="<?php echo base_url(); ?>c_upload/proses_dc">
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="title_dc">Digiceleb name (use all capital letters)</label>
+                            <input type="text" class="form-control" id="title_dc" name="title_dc" placeholder="Enter title" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="poster_dc">Poster</label>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="poster_dc" name="poster_dc" required>
+                                    <label class="custom-file-label" for="poster_dc">Choose file</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="bg_dc">Background</label>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="bg_dc" name="bg_dc" required>
+                                    <label class="custom-file-label" for="bg_dc">Choose file</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Headline Project</label>
+                            <textarea id="description_line1" class="form-control" rows="4" name="description_line1" placeholder="Enter description" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="description1">Digiceleb Description paragraph 1</label>
+                            <textarea id="description_line2" class="form-control" rows="4" name="description_line2" placeholder="Enter description" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="description2">Digiceleb Description paragraph 2</label>
+                            <textarea id="description_line3" class="form-control" rows="4" name="description_line3" placeholder="Enter description" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="image_dc1">Choose images 1</label>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="image_dc1" name="image_dc1" required>
+                                    <label class="custom-file-label" for="image_dc1">Choose file</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="image_dc2">Choose images 2</label>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="image_dc2" name="image_dc2" required>
+                                    <label class="custom-file-label" for="image_dc2">Choose file</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="instagram">Instagram link</label>
+                            <input type="text" class="form-control" id="instagram" name="instagram" placeholder="Enter instagram link" required>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
                 </form>
-              </div>
             </div>
-          </div>
         </div>
-      </section>
-
-            <!-- /.card -->
-            <!-- /.row -->
-            <!-- Main row -->
-
-            <!-- /.row (main row) -->
-          </div><!-- /.container-fluid -->
-        </section>
-        <!-- /.content -->
-      </div>
+    </div>
+</section>
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+    var inputs = document.querySelectorAll('.custom-file-input');
+    Array.prototype.forEach.call(inputs, function(input) {
+        input.addEventListener('change', function (e) {
+            var fileName = e.target.files[0].name;
+            var label = e.target.nextElementSibling;
+            label.innerText = fileName;
+        });
+    });
+});
+</script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+      
       <?php  $this->load->view('templates/footer_admin');?>
