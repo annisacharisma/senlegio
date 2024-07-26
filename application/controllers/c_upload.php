924e9a13ba85e7742e70f764c7c05c9ee89b48c6
gin-na <132482153+gin-na@users.noreply.github.com>
2024-07-26 22:39:33 +0700

    Update input dc c_upload.php

--- application/controllers/c_upload.php
+++ application/controllers/c_upload.php
@@ -71,4 +71,59 @@ class C_upload extends CI_Controller {
         $data['poster_adv'] = $this->db->get('advertising');
         $this->load->view('v_admin_advertising', $data);
     }
+
+    public function proses_dc()
+    {
+        $this->load->library('upload');
+
+        // Configure upload for each file
+        $config['upload_path'] = './assets/images/';
+        $config['allowed_types'] = 'jpg|png|mp4';
+        $config['encrypt_name'] = TRUE;
+
+        $this->upload->initialize($config);
+
+        $data = [];
+        $data['title_dc'] = $this->input->post('title_dc');
+        $data['description_line1'] = $this->input->post('description_line1');
+        $data['description_line2'] = $this->input->post('description_line2');
+        $data['description_line3'] = $this->input->post('description_line3');
+        $data['instagram'] = $this->input->post('instagram');
+
+        // Upload and save poster_dc
+        if ($this->upload->do_upload('poster_dc')) {
+            $upload_data = $this->upload->data();
+            $data['poster_dc'] = $upload_data['file_name'];
+        }
+
+        // Upload and save bg_dc
+        if ($this->upload->do_upload('bg_dc')) {
+            $upload_data = $this->upload->data();
+            $data['bg_dc'] = $upload_data['file_name'];
+        }
+
+        // Upload and save image_dc1
+        if ($this->upload->do_upload('image_dc1')) {
+            $upload_data = $this->upload->data();
+            $data['image_dc1'] = $upload_data['file_name'];
+        }
+
+        // Upload and save image_dc2
+        if ($this->upload->do_upload('image_dc2')) {
+            $upload_data = $this->upload->data();
+            $data['image_dc2'] = $upload_data['file_name'];
+        }
+
+        // Insert data into the database
+        $this->db->insert('digiceleb', $data);
+
+        // Redirect to the main page
+        redirect('c_data/data_digiceleb');
+    }
+    public function load_dc()
+    {
+        $data['poster_dc'] = $this->db->get('digiceleb')->result();
+        $this->load->view('v_admin_digiceleb', $data);
+    }
 }
+
