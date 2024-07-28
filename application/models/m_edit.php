616d2deb17c810f9b03c866f10c67586dd3cbe99
annisacharisma <129478564+annisacharisma@users.noreply.github.com>
2024-07-28 15:32:27 +0700

    Update m_edit.php

--- application/models/m_edit.php
+++ application/models/m_edit.php
@@ -13,6 +13,18 @@ class M_edit extends CI_Model {
         return $query->row();
     }
 
+    public function get_data_digiceleb($id_dc) {
+        $this->db->where('id_dc', $id_dc);
+        $query = $this->db->get('digiceleb');
+        return $query->row();
+    }
+
+    public function get_data_visualeffects($id_ve) {
+        $this->db->where('id_ve', $id_ve);
+        $query = $this->db->get('visual_effects');
+        return $query->row();
+    }
+
     public function edit_data_advertising($id_adv, $data) {
         $this->db->where('id_adv', $id_adv);
         $result = $this->db->update('advertising', $data);
@@ -21,5 +33,15 @@ class M_edit extends CI_Model {
     public function edit_data_animatedshorts($id_as, $data) {
         $this->db->where('id_as', $id_as);
         return $this->db->update('animated_shorts', $data);
+    }
+
+    public function edit_data_digiceleb($id_dc, $data) {
+        $this->db->where('id_dc', $id_dc);
+        return $this->db->update('digiceleb', $data);
+    }
+
+    public function edit_data_visualeffects($id_ve, $data) {
+        $this->db->where('id_ve', $id_ve);
+        return $this->db->update('visual_effects', $data);
     }
 }
