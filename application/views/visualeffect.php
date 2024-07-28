ba6fa96f5767200810616faab7119e9a924f16e3
gin-na <132482153+gin-na@users.noreply.github.com>
2024-07-28 13:26:31 +0700

    Update /images/ visualeffect.php

--- application/views/visualeffect.php
+++ application/views/visualeffect.php
@@ -46,7 +46,7 @@
                   ?>
         <div class="col-12 col-lg-3 d-flex flex-column justify-content-center align-items-center">
             <div class="image-wrapper">
-              <a href="<?php echo site_url('c_senlegio/show_ve_project/'.$u->id_ve); ?>"><img src="<?php echo base_url('assets/'.$u->poster_ve); ?>" style="height: 19rem; width: 15rem; object-fit: cover;"></a>
+              <a href="<?php echo site_url('c_senlegio/show_ve_project/'.$u->id_ve); ?>"><img src="<?php echo base_url('assets/images/'.$u->poster_ve); ?>" style="height: 19rem; width: 15rem; object-fit: cover;"></a>
                 <p class="mbr-text mbr-fonts-style mt-2 align-center display-7">
                     <strong><?php echo $u->title_ve?></strong> </p>
             </div>
@@ -63,26 +63,4 @@
     </div>
 </div>
   </section>
-    
-  <!-- <div class="container-fluid">
-    <div class="row justify-content-center" style="gap: 1;">
-        <div class="col-12 col-lg-3 d-flex flex-column justify-content-center align-items-center">
-            <div class="image-wrapper">
-              <a href="<?php echo base_url("c_senlegio/show_ve_project"); ?>"><img src="<?php echo base_url(); ?>assets/images/poster-1101x1573.jpg" style="height: 19rem; width: 15rem; object-fit: cover;"></a>
-                <p class="mbr-text mbr-fonts-style mt-2 align-center display-7">
-                    <strong>THE MOORS</strong> </p>
-            </div>
-        </div>
-        <div class="col-12 col-lg-3 d-flex flex-column justify-content-center align-items-center">
-            <div class="image-wrapper">
-                <img src="<?php echo base_url(); ?>assets/images/comingSoon.png" style="height: 19rem; width: 15rem; object-fit: cover;">
-                <p class="mbr-text mbr-fonts-style mt-2 align-center display-7">
-                    <strong>COMING SOON</strong></p>
-            </div>
-        </div>
-      </div>
-    </div>
-</div>
-  </section> -->
-
   <?php  $this->load->view('templates/Footer');?>
